6a302859329faed7b3696ffcad7d981456b08728
Zeyad-37 <zeyad.gasser@gmail.com>
2019-01-16 12:55:33 +0100

    itemInfo is parcelable now

--- gadapter/src/main/java/com/zeyad/gadapter/ItemInfo.kt
+++ gadapter/src/main/java/com/zeyad/gadapter/ItemInfo.kt
@@ -1,9 +1,14 @@
 package com.zeyad.gadapter
 
-data class ItemInfo(private val data: Any,
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.parcel.RawValue
+
+@Parcelize
+data class ItemInfo(private val data: @RawValue Any,
                     val layoutId: Int,
                     val id: Long = 0,
-                    var isEnabled: Boolean = true) {
+                    var isEnabled: Boolean = true) : Parcelable {
 
     fun <T> getData(): T {
         return data as T
