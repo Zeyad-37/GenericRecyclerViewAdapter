1157368d586bd18c0cd1ee043978b291a96f07b0
Zeyad-37 <zeyad.gasser@gmail.com>
2019-01-18 15:04:25 +0100

    implementing LayoutContainer for performance improvement

--- gadapter/src/main/java/com/zeyad/gadapter/GenericViewHolder.kt
+++ gadapter/src/main/java/com/zeyad/gadapter/GenericViewHolder.kt
@@ -2,8 +2,12 @@ package com.zeyad.gadapter
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import kotlinx.android.extensions.LayoutContainer
 
-abstract class GenericViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+abstract class GenericViewHolder<T>(itemView: View) : RecyclerView.ViewHolder(itemView), LayoutContainer {
+
+    override val containerView: View?
+        get() = itemView
 
     abstract fun bindData(data: T, isItemSelected: Boolean, position: Int, isEnabled: Boolean)
 }
