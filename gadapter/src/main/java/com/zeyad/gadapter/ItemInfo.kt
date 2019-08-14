43a2b61ce034b3b38a4b92c89b3d952c2147c35a
Zeyad-37 <zeyad.gasser@gmail.com>
2019-08-14 16:24:46 +0200

    updated deps Iteminfo T extends Parcelable

--- gadapter/src/main/java/com/zeyad/gadapter/ItemInfo.kt
+++ gadapter/src/main/java/com/zeyad/gadapter/ItemInfo.kt
@@ -2,10 +2,9 @@ package com.zeyad.gadapter
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
-import kotlinx.android.parcel.RawValue
 
 @Parcelize
-data class ItemInfo<T>(val data: @RawValue T,
-                       val layoutId: Int,
-                       val id: Long = 0,
-                       var isEnabled: Boolean = true) : Parcelable
+data class ItemInfo<T : Parcelable>(val data: T,
+                                    val layoutId: Int,
+                                    val id: Long = 0,
+                                    var isEnabled: Boolean = true) : Parcelable
